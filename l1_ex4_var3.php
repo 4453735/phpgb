c8fd28fc4eb01b95eb1343b69455328b3589ea37
Tim K <4453735@gmail.com>
2020-09-23 16:53:49 +0300

    Lesson 1

---
+++ l1_ex4_var3.php
@@ -0,0 +1,12 @@
+<?php
+$title = "Главная страница - страница обо мне";
+$h1 = "Информация обо мне";
+$year = date("Y");
+
+$content = file_get_contents("site_var3.html");
+
+$content = str_replace("{{ H1 }}", $h1, $content);
+$content = str_replace("{{ TITLE }}", $title, $content);
+$content = str_replace("{{ YEAR }}", $year, $content);
+
+echo $content;
